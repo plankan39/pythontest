cc5716f2d9965836a92e8da62e62c9d58b23a9c3
Lukas Elmlund <lukas.elmlund@gmail.com>
2020-11-30 22:06:17 +0100

    inital commit

---
+++ testscript.py
@@ -0,0 +1,14 @@
+from random import seed
+from random import randint
+a = float(1)
+print(a)
+
+
+if not(a > 4):
+    print(a)
+
+nmbrs = [randint(1, 101), randint(1, 101), randint(1, 101),
+         randint(1, 101), randint(1, 101), randint(1, 101)]
+
+for x in nmbrs:
+    print(x)
